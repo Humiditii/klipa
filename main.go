0411e1c5871ed510695b0cb714eea3f0991e00ff
Humiditii <humiditii45@gmail.com>
2025-05-27 19:56:01 +0100

    fix(): version

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/Humiditii/klipa/cmd"
 )
 
 
+var version = "dev"
 
 func main(){
 	cmd.Excecute()
+
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
+		fmt.Println("klipa version:", version)
+		return
+	}
 }
 
 
